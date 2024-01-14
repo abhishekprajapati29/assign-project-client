c93b08e0ce6219d9a187c92938faecb51e7d4fa6
abhicareer <abhishek1999.29@gmail.com>
2024-01-14 14:35:41 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/**
+=========================================================
+*  React - v2.2.0
+=========================================================
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducer from "./store/reducer/auth";
+import { createStore, compose, applyMiddleware } from "redux";
+import App from "./App"; // Make sure to provide the correct path to your App component
+
+import { MaterialUIControllerProvider } from "context";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
+const container = document.getElementById("app");
+const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <MaterialUIControllerProvider>
+        <App />
+      </MaterialUIControllerProvider>
+    </BrowserRouter>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
